4bd8860c2b3ca9afe7b43e003af55d1b87b15fcb
Brennon Nuckols <brennuck@gmail.com>
2020-04-01 16:24:12 -0600

    firstNamesAllCaps

--- index.js
+++ index.js
@@ -267,8 +267,13 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let roster = []
+
+  runners.map(runner => {
+    roster.push(runner.first_name.toUpperCase())
+  })
+  return roster
 }
 
 /**
