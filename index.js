f8050f39dfea2c4ee70d5bda4ed9ec5110b49c27
Brennon Nuckols <brennuck@gmail.com>
2020-04-01 16:22:06 -0600

    getFullNames

--- index.js
+++ index.js
@@ -246,8 +246,13 @@ function stringSmash(strings) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let roster = []
+
+  runners.map(runner => {
+    roster.push(`${runner.last_name}, ${runner.first_name}`)
+  })
+  return roster
 }
 
 /**
